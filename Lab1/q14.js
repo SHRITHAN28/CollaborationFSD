4bc79b68feb2d4ce04eb7880f911bb797dedfb78
Suryadevara Sai Shrithan <suryadevara.shrithan28@gmail.com>
2025-01-07 23:15:52 +0530

    validating all

--- Lab1/q14.js
+++
@@ -1,7 +0,0 @@
-/*Program: Create an Item class and a Cart class. The Cart class should have 
-methods to add, remove, and view items. The Item class should include 
-properties like name, price, and quantity.
-Task: Implement features like updating the quantity of items, calculating 
-the total price, and applying discount codes. Use localStorage to persist 
-the cart items even after refreshing the page. Display the cart contents 
-dynamically on the page. */
