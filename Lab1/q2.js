987316ab24040034908e303613b1e2c9d538bcf4
Suryadevara Sai Shrithan <suryadevara.shrithan28@gmail.com>
2025-01-07 21:47:36 +0530

    added files of all questions

---
+++ Lab1/q2.js
@@ -0,0 +1,5 @@
+//Program: Create a simple calculator using functions and variables. The user 
+//can input two numbers, and the calculator can add, subtract, multiply, or 
+//divide the numbers.
+//Task: Declare variables for the operands and the operation, and handle 
+//division by zero.
---
+++ Lab1/q3.js
@@ -0,0 +1,3 @@
+//Program: Create a Person object with properties like name, age, and a method 
+//greet() that prints a greeting message.
+//Task: Add a new property isAdult() to check if the person is an adult (age 18+).
