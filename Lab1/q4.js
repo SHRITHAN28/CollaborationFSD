c91e72507cded279bc20b2acabfce6e85a64c96e
Suryadevara Sai Shrithan <suryadevara.shrithan28@gmail.com>
2025-01-07 22:31:03 +0530

    testing another commit to main from vs code

---
+++ Lab1/q4.js
@@ -0,0 +1,3 @@
+//Program: Create a Student constructor function with properties like name, 
+//grade, and a method study(). Add a getGrade() method to get the student's grade.
+//Task: Create a few Student instances, call the study() method, and use getGrade().
