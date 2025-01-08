d75ab4795eaf4f2871f3c2a69ebe17d7c3d37c88
Sathvik1404 <yalakasr2004@gmail.com>
2025-01-08 09:23:35 +0530

    added new folder lab2

---
+++ Lab2/spread.js
@@ -0,0 +1,6 @@
+/*Write a function mergeArrays that accepts any number of arrays as arguments and
+merges them into a single array. Use the spread operator to accomplish this.
+Example Input:
+mergeArrays ([1, 2], [3, 4], [5, 6])
+Example Output:
+[1, 2, 3, 4, 5, 6] */
