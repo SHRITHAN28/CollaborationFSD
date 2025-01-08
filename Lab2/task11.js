b9f4eaae2c9500aeb8723ba94c53fa6ef75712ea
Suryadevara Sai Shrithan <111129725+SHRITHAN28@users.noreply.github.com>
2025-01-08 12:02:23 +0530

    comited task11

--- Lab2/task11.js
+++ Lab2/task11.js
@@ -2,3 +2,7 @@
 the values of two variables, a and b, without using a temporary variable.*/
 let arr = [1, 2, 3, 4, 5, 6]
 let [a, b] = arr.splice(1, 2)
+
+console.log(`Before swap: a = ${a}, b = ${b}`)
+[a, b] = [b, a]
+console.log(`After swap: a = ${a}, b = ${b}`)
