765d346468ba4d644a73c41f535d88b987f82cf1
shrithan28 <suryadevara.shrithan28@gmail.com>
2025-01-22 09:35:08 +0530

    lab 3 task 1

---
+++ Lab3/callback.js
@@ -0,0 +1,10 @@
+//Write a function greet that takes a name and a callback function.The function should return a greeting message by calling the callback with
+//the message.
+function greet(name, callback) {
+    const message = `Hello, ${name}!`;
+    callback(message);
+}
+function displayGreeting(greeting) {
+    console.log(greeting);
+}
+greet("John Doe", displayGreeting); // Output: Hello, John Doe!
